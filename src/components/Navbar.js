ef8abdc829f21e0a0f7ac6741b103c2f61f3ebbb
Michael Varrieur <mike.varrieur@gmail.com>
2020-01-25 11:33:26 -0500

    In progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
+import logo from "../img/yankee-logo.svg";
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,12 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img
+                src={logo}
+                className="logo"
+                alt="Yankee Trail"
+                style={{ width: "88px" }}
+              />
             </Link>
             {/* Hamburger menu */}
             <div
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -79,12 +79,6 @@ export const IndexPageTemplate = ({
                     </Link>
                   </div>
                 </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <div className="column is-12 has-text-centered"></div>
-                </div>
               </div>
             </div>
           </div>
